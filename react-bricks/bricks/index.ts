ae2d0a4027aa983e84ec3e7e0609fdd6b6cba92c
Manik A A Ngurah <manik.aangurah@gmail.com>
2022-08-26 15:23:33 +0200

    first-commit

--- react-bricks/bricks/index.ts
+++ react-bricks/bricks/index.ts
@@ -2,11 +2,15 @@ import { types } from 'react-bricks/frontend'
 import website from 'react-bricks-ui/website'
 import blog from 'react-bricks-ui/blog'
 import HeroUnit from './MyHeroUnit'
+import Thumbnail from './Thumbnail'
+import Gallery from './Gallery'
 
 const bricks: types.Brick<any>[] = [
   ...website, // React Bricks UI
   ...blog,
   HeroUnit, // Example custom brick
+  Thumbnail,
+  Gallery
   // Put here your other bricks...
 ]
 
